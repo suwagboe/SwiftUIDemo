954df5487f9afd1b0ac5b12869b1b148f438ec10
World Domination a Brand <worlddomination@Worlds-MacBook-Pro.local>
2019-09-24 15:52:25 -0400

    added xcode project

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by World Domination a Brand on 9/24/19.
+//  Copyright © 2019 Pursuitful stuff. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hey Mama")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
